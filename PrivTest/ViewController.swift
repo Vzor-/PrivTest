bf5bc1f5af3d286363a6ecfa9616ea96e5e9aca6
Vzor- <Kyle@Berezin.com>
2021-03-25 14:54:34 -0400

    Initial Commit

---
+++ PrivTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PrivTest
+//
+//  Created by Kyle Berezin on 3/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
